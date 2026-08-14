2b96d6174647ffd425ce1d859f7c2e2d96b40d7d
Tres Sims <tres.sims95@gmail.com>
2026-08-13 19:35:36 -0500

    Expose tui

--- tui/tab.go
+++ tui/tab.go
@@ -4,10 +4,13 @@ import "charm.land/lipgloss/v2"
 
 // Tab is an interface for rendering a tab page for the scry tui
 type Tab interface {
-	// Render takes [w]idth and [h]eight and the default style.
+	// Render takes (w)idth and (h)eight and the default style.
 	// returns a string filling those dimensions, and optionally using that style
 	Render(w, h int, style lipgloss.Style) string
 
-	// Title returns the name of the tab
+	// Load implements [Option]
+	Load(m *Model)
+
+	// Name returns the name of the tab
 	Name() string
 }
